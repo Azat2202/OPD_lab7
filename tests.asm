6c59bceea2d81e5a18dbde6489d60919334d25fb
Azat2202 <azat222@gmail.com>
2023-06-07 21:00:52 +0300

    fix: not fixed yet :(

--- tests.asm
+++ tests.asm
@@ -1,68 +1,73 @@
-ORG 0x500
+ORG 0x501
 ;Program that run sppecified tests.
 ;Features: it needs only test addresses 
 ;Programms must handle 0 args and return 0 or 1 in AC
-;Constants: 
-TEST_COUNT:	WORD	0x4							;Tests amount
-TESTS:		WORD	0x010, 0x50, 0x100, 0x150	;Test addresses
-RESULTS:	WORD	4 DUP (?)					;Test results
-MAIN_RES:	WORD	?							;Result over all tests
+;----------------------------Constants------------------------------- 
+TEST_COUNT:	WORD	0x4			;Tests amount
+TESTS:	WORD	0x010, 0x50, 0x100, 0x150	;Test addresses
+RESULTS:	WORD	4 DUP (?)			;Test results
+MAIN_RES:	WORD	?			;Result over all tests
+ARRAY_ADDR:	WORD	0x501			;Address of first element
+;---------------------------------------------------------------------
 
-first_test:	WORD	0x501			;First test address 
-next_test:	WORD	?				;Next test for call
-next_test_b:WORD	?				;Buffer for CALL (next_test)+
-next_res:	WORD 	?				;Next result address
-loop_buf:	WORD	?				;Loop cell
-arr_addr:	WORD	?				;Array of test addresses pointer
-START:		LD		TEST_COUNT		;Save from constant test count to loop cell
-			ST		loop_buf		
-			LD		first_test		;Save from constant first test cell to next_test cell
-			ST		next_test	
-			ADD		TEST_COUNT 		;Count results cells
-			ST		next_res	
-make_test:	LD		(next_test)+	;This code immitates CALL (next_test)+
-			ST		next_test_b
-			CALL	next_test_b
-			ST		(next_res)+		;Save result 
-			LOOP 	loop_buf
-			JUMP 	make_test
-			LD		TEST_COUNT
-			ST		loop_buf
-check_res:	LD		-(next_res)		;Check test
-			ROR
-			BCC		test_fail		;If test failed go to test failed
-			LOOP	loop_buf
-			JUMP	check_res
-test_ok:	LD		#0x1
-			ST		MAIN_RES
-			HLT
-test_fail:	LD		#0x1
-			ST		MAIN_RES
-			HLT
-			
-			
+test_loop:	WORD	?	;Test loop variable
+array_adr:	WORD	?	;Next element in test array
+result_adr:	WORD	?	;Next element in result array
+test_adr:	WORD	?	;Next test address
+START:	LD	TEST_COUNT
+	ST	test_loop
+	LD	ARRAY_ADDR
+	ST	array_adr
+	ADD	TEST_COUNT
+	ST	result_adr
+MAIN:	LD	(array_adr)+
+	ST	test_adr
+	CALL	test_adr
+	ST	(result_adr)+
+	LOOP	test_loop
+	JUMP	MAIN
+	JUMP	CHECK_RES
+CHECK_RES:	LD	TEST_COUNT
+	ST	test_loop
+CHECKING:	LD	-(result_adr)
+	ROR
+	BCC	TEST_FAIL
+	LOOP	test_loop
+	JUMP	CHECKING
+	JUMP	TEST_OK
+TEST_OK:	LD	#0x1
+	ST	MAIN_RES
+	HLT
+TEST_FAIL:	LD	#0x0
+	ST	MAIN_RES
+	HLT
+	
+	
+	
+	
+	
 ;TEST 2
-			ORG		0x10
-			LD		#0x1
-			RET
+	ORG	0x10
+	LD	#0x1
+	RET
 
 ;TEST 3
-			ORG		0x50
-			LD		#0x1
-			RET
+	ORG	0x50
+	LD	#0x1
+	RET
 
 ;TEST 4
-			ORG		0x100
-			LD		#0x1
-			RET
+	ORG	0x100
+	LD	#0x1
+	RET
 
 ;TEST 5
-			ORG		0x150
-			LD		#0x1
-			RET
+	ORG	0x150
+	LD	#0x1
+	RET
 
 
-			
+	
 
 
 
